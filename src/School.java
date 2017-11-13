02f6bd8048fcce9d9415467730b6879dd2cdf0f3
Nadya <nadya.ilinskiy@gmail.com>
2017-11-12 18:58:08 -0800

    Adding solution for class School + class Student

--- src/School.java
+++ src/School.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 
+
 public class School {
 	
 	//Variables for this class
-	ArrayList<Book> catalog = new ArrayList<Book>();
+	Library library;
+	ArrayList<Student> students = new ArrayList<Student>();
 	int numStudents = 0;
-	String name;
+	String name = "Unknown School";
+	int MAX_STUDENTS = 0;
 
 	/**
 	 * Start the execution of your program here.
@@ -29,10 +32,28 @@ public class School {
 	 */
 	public static void main(String args[]) {
 		//TODO create a school (similar to the library)
+		School mySchool = new School("Hogwarts", 7500);
+		
+		mySchool.AddSingleStudent("Harry", "Potter", Student.Gender.MALE, 1);
+		
+		mySchool.AddSingleStudent("Draco", "Malfoy", Student.Gender.MALE, 1);
+		
+		mySchool.AddSingleStudent("Hermy", "Granger", Student.Gender.FEMALE, 1);
+		
+		mySchool.AddSingleStudent("Fred", "Wesley", Student.Gender.MALE, 3);
+		
+		mySchool.AddSingleStudent("George", "Wesley", Student.Gender.MALE, 3);
+		
+		mySchool.AddSingleStudent("Serge", "Ilinskiy", Student.Gender.MALE, 3);
+		
+		mySchool.removeSingleStudent();
 		
 		//TODO Print the toString method in school. Such as, school.toString()
+		System.out.println(mySchool.toString());
 		
 		//TODO Print any other variables that make sense for a school. Be creative.
+				
+		System.out.println(mySchool.getInfo());
 	}
 	
 	/**
@@ -40,8 +61,10 @@ public class School {
 	 * this.name refers to the global variable for the class. String name is the name we pass in when building the school
 	 * @param name
 	 */
-	public School(String name) {
+	public School(String name, int maxStudents) {
 		this.name = name;
+		this.MAX_STUDENTS = maxStudents;
+		this.library = new Library ();
 	}
 	
 	/**
@@ -49,15 +72,22 @@ public class School {
 	 */
 	public String toString() {
 		//TODO Write a method to return a list of all books at the school
-		return "TODO";
+		String returnString = "School: '" + this.name + "'\n  School library:\n";	
+		returnString += library;
+		
+		return returnString;
 	}
 	
 	/**
 	 * adds a single student to the school
 	 */
-	public void AddSingleStudent() {
-		numStudents++;
-		//Do I need a MAX_STUDENTS ???
+	public void AddSingleStudent(String firstName, String lastName, Student.Gender gender, int grade) {
+		if(numStudents < MAX_STUDENTS) {
+			numStudents++;
+			students.add(new Student (firstName, lastName, gender, grade));
+			
+		}
+		//Do I need a MAX_STUDENTS ??? Yees? 
 	}
 	
 	/**
@@ -66,6 +96,8 @@ public class School {
 	public void removeSingleStudent() {
 		if(numStudents > 0) {
 			numStudents--;
+			students.remove(students.size()-1);
+			
 		}
 	}
 	
@@ -76,5 +108,15 @@ public class School {
 	 */
 	public int getStudents() {
 		return numStudents;
+	}
+	
+	public String getInfo() {
+		String returnString = "School: '" + this.name + "', number of students " + this.numStudents + " of capasity " + this.MAX_STUDENTS;
+		
+		returnString += "\nStudents:\n";
+		for (Student s : students ) {
+			returnString += "    " + s + "\n";
+		}
+		return returnString;
 	}
 }
